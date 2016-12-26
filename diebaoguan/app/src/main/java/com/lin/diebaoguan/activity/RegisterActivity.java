2eb5d1b7377aff97303c2c4b8621354870f73bcc
JianFeng <qtdh3@163.com>
2016-12-26 16:05:51 +0800

    添加注册页

---
+++ diebaoguan/app/src/main/java/com/lin/diebaoguan/activity/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.lin.diebaoguan.activity;
+
+import android.os.Bundle;
+
+import com.lin.diebaoguan.BaseActivity;
+import com.lin.diebaoguan.R;
+
+/**
+ * It's Created by NewLand-JianFeng on 2016/12/26.
+ */
+
+public class RegisterActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
