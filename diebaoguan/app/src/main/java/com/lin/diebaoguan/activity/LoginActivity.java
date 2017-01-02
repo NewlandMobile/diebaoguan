23aa7b63a91e7754e5328443e012e13561d7080f
JF-YouXia <2208594696@qq.com>
2017-01-02 13:53:19 +0800

    登录页 UI

---
+++ diebaoguan/app/src/main/java/com/lin/diebaoguan/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.lin.diebaoguan.activity;
+
+import android.os.Bundle;
+
+import com.lin.diebaoguan.BaseActivity;
+import com.lin.diebaoguan.R;
+
+/**
+ * Created by Alan on 2017/1/2 0002.
+ * 登录页
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
