e134e7bf1f69c2c4ccd31f694791d7a1c45f1049
xiaolinxiao <1057705307@qq.com>
2017-01-04 18:22:33 +0800

    git GUI test

---
+++ diebaoguan/app/src/main/java/com/lin/diebaoguan/adapter/CommentAdapter.java
@@ -0,0 +1,32 @@
+package com.lin.diebaoguan.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by linx on 2017/1/415:14.
+ * mail :1057705307@QQ.com.
+ * describe:评论适配器
+ */
+public class CommentAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- diebaoguan/app/src/main/java/com/lin/diebaoguan/common/HttpUtils.java
+++ diebaoguan/app/src/main/java/com/lin/diebaoguan/common/HttpUtils.java
@@ -41,11 +41,11 @@ import java.util.Map;
  *
  * @author Ognyan Bankov
  */
-public class HTTPUtils {
+public class HttpUtils {
     private static RequestQueue mRequestQueue;
-    private final static String tag = HTTPUtils.class.getName();
+    private final static String tag = HttpUtils.class.getName();
 
-    private HTTPUtils() {
+    private HttpUtils() {
     }
 
     public static RequestQueue getmRequestQueue() {
@@ -53,7 +53,7 @@ public class HTTPUtils {
     }
 
     public static void setmRequestQueue(RequestQueue mRequestQueue) {
-        HTTPUtils.mRequestQueue = mRequestQueue;
+        HttpUtils.mRequestQueue = mRequestQueue;
     }
 
     public static void init(Context context) {
