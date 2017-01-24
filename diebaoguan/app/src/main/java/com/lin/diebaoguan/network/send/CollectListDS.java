a514a32099254cc3493fbd332b6a80ada5b46336
JianFeng <qtdh3@163.com>
2017-01-24 10:13:58 +0800

    重新加回来

---
+++ diebaoguan/app/src/main/java/com/lin/diebaoguan/network/send/CollectListDS.java
@@ -0,0 +1,37 @@
+package com.lin.diebaoguan.network.send;
+
+/**
+ * Created by linx on 2017/1/1911:09.
+ * mail :1057705307@QQ.com.
+ * describe:收藏列表
+ */
+public class CollectListDS extends BaseSendTemplate {
+    private String cid;
+    private String uid;
+    private String offset;
+    private String rows;
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getOffset() {
+        return offset;
+    }
+
+    public String getRows() {
+        return rows;
+    }
+
+    public void setOffset(String offset) {
+        this.offset = offset;
+    }
+
+    public void setRows(String rows) {
+        this.rows = rows;
+    }
+}
